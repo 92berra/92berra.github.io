55168fa273d288df05e764a00b3fbe0e5ea5e47c
92berra <ohyunwha@gmail.com>
2024-07-10 19:23:09 +0900

    Edit: Portfolio > GAN URL

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,13 +12,13 @@ export default function Portfolio() {
             <br/>
 
             <div align='center'>
-                <h3 className='ContentHeader'>Vanilla GAN with FashionMNIST</h3>
-                M3 Macbook Pro / VSCode / PyTorch(mps) 2.3.1
+                <h3 className='ContentHeader'>GANs Tutorial</h3>
+                M3 Macbook Pro / macOS Sonoma 14.5 / VSCode / PyTorch(mps) 2.3.1
 
                 <br/>
                 <br/>
                 
-                <a href='https://github.com/92berra/Selfstudy/tree/main/pytorch/vanillagan-fashionmnist'>
+                <a href='https://github.com/92berra/GANs-tutorial'>
                     <img src={sample_animation}/>
                 </a>
             </div>
@@ -31,7 +31,7 @@ export default function Portfolio() {
             <div align='center'>
                 <h3 className='ContentHeader'>Location-based Hangul Component Separation <br/>
                 for Accuracy Improvement of Korean Font Generation Model</h3>
-                Tensorflow-gpu 1.13 / opencv-python / Few-shot Font Generation
+                Ubuntu LTS 14.02 / NVIDIA GeForce / VSCode / Tensorflow-GPU 1.13 / Opencv-python
 
                 <br/>
                 <br/>
